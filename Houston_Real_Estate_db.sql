3f932f5db3bc45e6bd2b7f4e0e15350c0b809d5f
Ellenrud84 <ellenrud84@gmail.com>
2020-07-30 19:18:18 -0500

    updated jupyter notebook and sql code to reflect changes to normalize per erd

--- Houston_Real_Estate_db.sql
+++ Houston_Real_Estate_db.sql
@@ -1,6 +1,8 @@
 DROP TABLE real_acct;
+drop table tax_year_2019;
+drop table tax_year_2018;
 drop table sq_ft;
-drop TABLE neighborhoods 
+drop TABLE neighborhoods ;
 
 
 -- Create tables for raw data to be loaded into
@@ -8,15 +10,28 @@ CREATE TABLE real_acct (
 	id serial primary key,
 	ACCOUNT BIGINT,
 	TAX_YEAR INT,
-	SITE_ADDR_1 varchar(300),
-	SITE_ADDR_2 varchar(100),
-	SITE_ADDR_3 varchar(300),
+	full_addr varchar(300),
+	SITE_ADDR_3 numeric,
 	STATE_CLASS varchar(10),
 	NEIGHBORHOOD_CODE NUMERIC,
 	NEIGHBORHOOD_GROUP INT,
 	ACREAGE NUMERIC,
-	TOTAL_APPRAISED_VALUE NUMERIC
+	NEW_OWNER_DATE DATE
 );
+
+CREATE TABLE tax_year_2018 (
+	id serial primary key,
+	account bigint,
+	land_value numeric,
+	total_appraised_value numeric,
+	total_market_value numeric);
+	
+CREATE TABLE tax_year_2019 (
+	id serial primary key,
+	account bigint,
+	land_value numeric,
+	total_appraised_value numeric,
+	total_market_value numeric);
 
 CREATE TABLE sq_ft (
 	id serial primary key,
@@ -28,4 +43,3 @@ CREATE TABLE neighborhoods (
 	neighborhood_code numeric,
 	neighborhood_group numeric,
 	neighborhood varchar(300));
-
